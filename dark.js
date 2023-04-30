c0ade2ceccad25fb88c162bb6d7f663e00a717ae
Ragnarzeiro <84497786+Ragnarzeiro@users.noreply.github.com>
2023-04-30 01:05:20 -0300

    Update dark.js

--- dark.js
+++ dark.js
@@ -4,7 +4,11 @@ function toggleMode() {
 
   if (body.classList.contains("dark-mode")) {
     body.classList.remove("dark-mode");
+    body.classList.add("light-mode");
     btn.innerText = "Modo Escuro";
+  } else if (body.classList.contains("light-mode")) {
+    body.classList.remove("light-mode");
+    btn.innerText = "Modo Claro";
   } else {
     body.classList.add("dark-mode");
     btn.innerText = "Modo Claro";
